72453fed555e1dd0ac61037e864e88c2aa642254
brianna219c <briannaw@csedge.org>
2020-06-20 13:01:02 -0700

    added firebase

--- app/src/main/java/com/example/edacbasicapp/FormActivity.java
+++ app/src/main/java/com/example/edacbasicapp/FormActivity.java
@@ -7,12 +7,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class FormActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class FormActivity extends AppCompatActivity {
+    FirebaseDatabase fdatabase;
+    DatabaseReference rdatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+        fdatabase = FirebaseDatabase.getInstance();
+        rdatabase = fdatabase.getReference();
     }
     public void openFormPage(View view) {
         Intent myIntent = new Intent(FormActivity.this, FormActivity.class);
